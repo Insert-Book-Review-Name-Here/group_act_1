c2feba120ffa165999483db6120c990d28a0ef20
Gabriel Alpha Aguilar <JS-20570463@SERVER2.DOMAIN>
2023-10-07 15:59:29 +0800

    List page

---
+++ ASI.Basecode.WebApp/Models/PersonModel.cs
@@ -0,0 +1,10 @@
+﻿namespace ASI.Basecode.WebApp.Models
+{
+    public class PersonModel
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string WonItem { get; set; }
+        public string Address { get; set; }
+    }
+}
